53d293797b8119a67f191648aa941087d4243fcf
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-15 14:57:49 -0300

    finalizando o portfólio

---
+++ JS/javascript.js
@@ -0,0 +1,55 @@
+/*  MOUSE OUT  */ 
+function titleOver (){
+    document.getElementById('t-html').innerText="";
+    document.getElementById('t-css').innerText="";
+    document.getElementById('t-js').innerText="";
+    document.getElementById('t-php').innerText="";
+    document.getElementById('t-asp').innerText="";
+    document.getElementById('t-mysql').innerText="";
+    document.getElementById('t-sqlserver').innerText="";
+    document.getElementById('t-bootstrap').innerText="";
+    document.getElementById('t-angular').innerText="";
+    document.getElementById('t-jquery').innerText="";
+}
+
+
+/*  MOUSE OVER  */ 
+function titleHtml() {
+    document.getElementById('t-html').innerText="HTML5";
+}
+
+function titleCSS() {
+    document.getElementById('t-css').innerText="CSS3";
+}
+
+function titleJS() {
+    document.getElementById('t-js').innerText="JavaScript";
+}
+
+function titlePHP () {
+    document.getElementById('t-php').innerText="PHP";
+}
+
+function titleASP () {
+    document.getElementById('t-asp').innerText=".Net";
+}
+
+function titleMySQL () {
+    document.getElementById('t-mysql').innerText="MySql";
+}
+
+function titleSqlServer () {
+    document.getElementById('t-sqlserver').innerText="SqlServer";
+}
+
+function titleBootstrap () {
+    document.getElementById('t-bootstrap').innerText="Bootstrap5";
+}
+
+function titleAngular () {
+    document.getElementById('t-angular').innerText="Angular";
+}
+
+function titleJquery () {
+    document.getElementById('t-jquery').innerText="JQuery";
+}
